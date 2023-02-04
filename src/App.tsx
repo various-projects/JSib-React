c6f91a06144b2dbb8e78482dce0887a738c1b59a
Nobody <nobody@example.com>
2023-02-04 04:29:38 +0300

    Add React routing Remove stub text

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { SideBar } from './components/sidebar/SideBar';
 import { PostForm } from './components/postForm/PostForm';
+import { Routes } from 'react-router-dom';
+import { Route } from 'react-router';
 
 const setCSS = (styleName: string) => { };
 
@@ -12,49 +14,16 @@ function App() {
 
       <PostForm />
 
+      <Routes>
+        <Route path="/:boardId" element={<h1>Board</h1>} />
+        <Route path="/:boardId/:threadId/:messageId?" element={<h1>Thread</h1>} />
+      </Routes>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
       </header>
     </div>
   );
